8b43e449c6a9c55143d6ea0175b7154070c5ef31
Tomasz Kosiński <azewiusz@gmail.com>
2023-10-19 23:03:09 +0200

    Added example2 directory

---
+++ example2/gen_tests.bat
@@ -0,0 +1,1 @@
+crosshair cover --example_output_format=pytest --coverage_type opcode --per_condition_timeout=15 main.named_assertion_error > ./tests/tests_with_exceptions_after_diff.py
---
+++ example2/main.py
@@ -0,0 +1,3 @@
+def named_assertion_error(a: int):
+    assert a > 100, "This is assertion error that should appear in test case match - first checkpoint"
+    assert a > 160, "This is assertion error that should appear in test case match - second checkpoint"
